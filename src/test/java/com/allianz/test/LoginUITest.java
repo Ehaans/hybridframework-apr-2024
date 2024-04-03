4684c73450cc7553c44f21df6f81e75c338162ed
shibin-basheer1 <shibinb1988@gmail.com>
2024-04-02 18:58:25 -0700

    Created 1 TestNG test Methos

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,13 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	
+	@Test
+	public void titleTest()
+	{
+		System.out.println("title test");
+	}
+
+}
